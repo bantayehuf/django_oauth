25e0bef44ef4a4bdfb99773fabb7702b0d6b13cc
Bantayehu Fikadu <bantayehuf@gmail.com>
2021-06-08 18:36:15 +0300

    First Commit
    -Start new project
    -Setup mariadb database as default database
    -Setup custom user model

---
+++ oauth/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms.user_form import UserAdminCreationForm, UserAdminChangeForm
+
+User = get_user_model()
+
+class UserAdmin(BaseUserAdmin):
+    # The forms to add and change user instances
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ['get_full_name', 'email']
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('Important Dates', {'fields': ('date_joined', 'last_login')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('AddUser',),
+            'fields': ('first_name', 'last_name', 'email', 'password1', 'password2')}
+         ),
+    )
+    
+    search_fields = ('email','first_name', 'last_name')
+    ordering = ['first_name', 'last_name', 'email']
+    #filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, UserAdmin)
---
+++ oauth/models/__init__.py
@@ -0,0 +1,2 @@
+from .user_account import User
+from . import sample
